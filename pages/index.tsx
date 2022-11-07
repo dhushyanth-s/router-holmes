7331da8b92a48e9e56ff5f34a603bf92cab5b909
Dhushyanth S <dhushyanth5602@gmail.com>
2022-11-08 04:57:30 +0530

    Styled the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,39 @@
-import { Sidebar } from '../components/sidebar';
-import { styled } from '../stitches.config';
+import { Sidebar } from "../components/sidebar";
+import { styled } from "../stitches.config";
 
-const Text = styled('p', {
-  fontFamily: '$system',
-  color: '$hiContrast',
+const Text = styled("p", {
+  fontFamily: "$system",
+  color: "$hiContrast",
 
   variants: {
     size: {
       1: {
-        fontSize: '$1',
+        fontSize: "$1",
       },
       2: {
-        fontSize: '$2',
+        fontSize: "$2",
       },
       3: {
-        fontSize: '$3',
+        fontSize: "$3",
       },
     },
   },
 });
 
+const Base = styled("main", {
+  width: "100%",
+  height: "100vh",
+  maxHeight: "100vh",
+  background: "#04040E",
+  position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  color: "white",
+  fontFamily: "Poppins",
+});
+
 export default function Home() {
-  return (
-    <Text as="h1" size="3">
-		
-      Welcome
-    </Text>
-  );
+  return <Base>Welcome! Use the navbar on the left to navigate.</Base>;
 }
